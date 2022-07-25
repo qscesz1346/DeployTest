264a76e41e9afe30aaa44b10bb9cdbf5562298b7
jungwoogyun <jwg8910@naver.com>
2022-07-25 13:37:55 +0900

    ff

---
+++ src/main/java/ShowBusInfo/C01ShowBusInfo.java
@@ -0,0 +1,43 @@
+package ShowBusInfo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ShowBus.Info")
+public class C01ShowBusInfo extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		req.setCharacterEncoding("UTF-8");
+		resp.setContentType("application/json; charset=utf-8");
+		
+		String addr = "http://apis.data.go.kr/1613000/ExpBusInfoService/getExpBusTrminlList?serviceKey=1w828es7tjmBRwseETkFg4yxq%2BOIBObFHWzx01a4kMKh7sQFDNXqaLpNfyJflswbz5BNCIf0gvzy4cUjBetNyw%3D%3D&pageNo=1&numOfRows=230&_type=json";
+		
+		URL url = new URL(addr);
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream(),"UTF-8"));
+		StringBuffer sb = new StringBuffer();
+		String str = null;
+		
+		while(true) {
+			str=br.readLine();
+			if(str==null)
+				break;
+			sb.append(str);
+		}
+		br.close();
+	
+		resp.getWriter().write(sb.toString());
+		
+	}
+
+	
+}
---
+++ src/main/webapp/APITest/OpenAPI/01JSON.jsp
@@ -0,0 +1,60 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<%@include file="/resources/includes/link.jsp" %>
+
+<script>
+
+	$.ajax({
+		type : "GET",
+		dataType : "json",
+		url : "/ShowBus.Info",
+		contentType : "application/json",
+		success : function(result){
+			//console.log(result);
+			$.each(result,function(k1,v1){
+			
+				$.each(v1,function(k2,v2){
+						
+					if(k2==="body"){
+					
+						$.each(v2,function(k3,v3){
+							 
+							if(k3==="items"){
+								
+								$.each(v3,function(k4,v4){
+									
+									for(i=0;i<v4.length;i++){
+										document.write(v4[i].terminalNm+" "+v4[i].terminalId+"<br>");	
+									}
+									
+									
+								})
+								
+								
+							}
+						})
+						
+					}
+					
+				})
+				
+			})
+			
+			
+		},
+		error : function(request,status,error){
+			
+		}
+	})
+</script>
+
+
+</body>
+</html>
